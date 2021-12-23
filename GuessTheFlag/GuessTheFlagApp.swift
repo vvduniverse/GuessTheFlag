62acccbf5e861868c51b314dff7ea4e1c1ed4c20
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-23 14:40:24 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vahtee Boo on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
