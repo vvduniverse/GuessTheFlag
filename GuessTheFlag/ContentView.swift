babc877bbacd22010b1a4a3379f71ad7e05ade89
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 19:52:59 +0200

    3.2d

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,23 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                Color.blue
-                Color.init(red: 0.23, green: 0.45, blue: 0.24, opacity: 0.7)
-                    .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: .infinity, maxHeight: 250)
-                Text("Fuck machine")
-                    .font(.title)
-                Spacer()
+        ZStack {
+            VStack(spacing: 0) {
+                ZStack {
+                    Color.blue
+                    Color.init(red: 0.23, green: 0.45, blue: 0.24, opacity: 0.7)
+                        .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: .infinity, maxHeight: 250)
+                    Text("Fuck machine")
+                        .font(.title)
+                    Spacer()
+                }
+                
+                ZStack {
+                    Color.primary
+                    Color.yellow
+                        .frame(width: 300, height: 300)
+                    Color.blue
+                        .frame(width: 200, height: 200)
+                    Color.secondary
+                        .frame(width: 100, height: 100)
+                    Image(systemName: "sun.min")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
             }
-            
-            ZStack {
-                Color.primary
-                Color.blue
-                    .frame(width: 200, height: 200)
-                Color.secondary
-                    .frame(width: 100, height: 100)
-            }
+            Text("Another girl")
+                .padding()
+                .background(.ultraThinMaterial)
         }
         .ignoresSafeArea()
     }
