8eec9481f702749ef92e2154c1887cb8b9de2ed2
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 19:41:57 +0200

    3d

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
-            VStack {
-                Color.green
-                    .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: 120, maxHeight: 250)
+        VStack(spacing: 0) {
+            ZStack {
+                Color.blue
+                Color.init(red: 0.23, green: 0.45, blue: 0.24, opacity: 0.7)
+                    .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: .infinity, maxHeight: 250)
                 Text("Fuck machine")
+                    .font(.title)
                 Spacer()
             }
+            
+            ZStack {
+                Color.primary
+                Color.blue
+                    .frame(width: 200, height: 200)
+                Color.secondary
+                    .frame(width: 100, height: 100)
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
