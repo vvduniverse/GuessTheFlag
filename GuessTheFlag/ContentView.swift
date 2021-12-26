f50c23d69d7849a210fe6e628ace85bbb04d6134
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-26 12:01:29 +0200

    Six to nine part

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,39 +7,35 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         ZStack {
-            VStack(spacing: 0) {
-                ZStack {
-                    Color.blue
-                    Color.init(red: 0.23, green: 0.45, blue: 0.24, opacity: 0.7)
-                        .frame(minWidth: 40, idealWidth: 100, maxWidth: 200, minHeight: 50, idealHeight: .infinity, maxHeight: 250)
-                    Text("Fuck machine")
-                        .font(.title)
-                    Spacer()
-                }
-                
-                ZStack {
-                    Color.primary
-                    Color.yellow
-                        .frame(width: 300, height: 300)
-                    Color.blue
-                        .frame(width: 200, height: 200)
-                    Color.secondary
-                        .frame(width: 100, height: 100)
-                    Image(systemName: "sun.min")
-                        .resizable()
-                        .frame(width: 75, height: 75)
+            Color.blue
+                .opacity(0.6)
+                .ignoresSafeArea()
+        VStack(spacing: 30) {
+            VStack {
+                Text("Tap the flag of")
+                    .foregroundColor(.white)
+                Text(countries[correctAnswer])
+                    .foregroundColor(.white)
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                    
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
                 }
             }
-            Text("Another girl")
-                .font(.title).bold()
-                .foregroundStyle(.secondary)
-                .padding()
-                .background(.ultraThinMaterial)
         }
-        .ignoresSafeArea()
+    }
     }
 }
 
